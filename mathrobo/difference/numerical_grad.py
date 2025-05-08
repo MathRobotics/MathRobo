a57aa069973951fecf03bd8964a81f0528d0d515
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-05-08 18:17:58 +0900

    👌 IMPROVE:

--- mathrobo/difference/numerical_grad.py
+++ mathrobo/difference/numerical_grad.py
@@ -7,18 +7,21 @@ import numpy as np
 def numerical_grad(x, func, eps=1e-8, method="central", sub_func = None):
     x = np.asarray(x, dtype=float).flatten()
     
-    y0 = np.atleast_1d(func(x))
+    y0 = func(x)
+
     if sub_func is not None:
         dy = sub_func(y0, y0)
+        y_size = dy.size
         grad = np.zeros((dy.size, x.size))
     else:
+        y_size = y0.size
         grad = np.zeros((y0.size, x.size))
 
     for i in range(x.size):
         x1 = x.copy()
         if method == "forward":
             x1[i] += eps
-            y1 = np.atleast_1d(func(x1))
+            y1 = func(x1)
             if sub_func is not None:
                 grad[:, i] = sub_func(y0, y1) / eps
             else:
@@ -28,10 +31,10 @@ def numerical_grad(x, func, eps=1e-8, method="central", sub_func = None):
             x1[i] += eps
             x2 = x.copy()
             x2[i] -= eps
-            y1 = np.atleast_1d(func(x1))
-            y2 = np.atleast_1d(func(x2))
+            y1 = func(x1)
+            y2 = func(x2)
             if sub_func is not None:
-                grad[:, i] = sub_func(y0, y1) / (2 * eps)
+                grad[:, i] = sub_func(y2, y1) / (2 * eps)
             else:
                 grad[:, i] = (y1 - y2) / (2 * eps)
 
@@ -41,10 +44,10 @@ def numerical_grad(x, func, eps=1e-8, method="central", sub_func = None):
             x_m1h = x.copy(); x_m1h[i] -= 1 * eps
             x_p1h = x.copy(); x_p1h[i] += 1 * eps
             x_p2h = x.copy(); x_p2h[i] += 2 * eps
-            f_m2h = np.atleast_1d(func(x_m2h))
-            f_m1h = np.atleast_1d(func(x_m1h))
-            f_p1h = np.atleast_1d(func(x_p1h))
-            f_p2h = np.atleast_1d(func(x_p2h))
+            f_m2h = func(x_m2h)
+            f_m1h = func(x_m1h)
+            f_p1h = func(x_p1h)
+            f_p2h = func(x_p2h)
             if sub_func is not None:
                 t1 = 8 * sub_func(f_p1h, f_m1h)   # Δ = f_m1h - f_p1h
                 t2 = sub_func(f_p2h, f_m2h)   # Δ = f_m2h - f_p2h
@@ -55,4 +58,4 @@ def numerical_grad(x, func, eps=1e-8, method="central", sub_func = None):
         else:
             raise ValueError(f"Unsupported method: {method}")
 
-    return grad if y0.size > 1 else grad.flatten()
+    return grad if y_size > 1 else grad.flatten()
