bfbbce1186a042ec769bff097e72245115341b0f
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-07-15 18:47:04 +0900

    👌 IMPROVE:

--- mathrobo/lie/lie_abst.py
+++ mathrobo/lie/lie_abst.py
@@ -1,22 +1,66 @@
+
+
+from typing import Union
+from abc import ABC, abstractmethod
+
 from ..basic import *
 
-class LieAbstract:
-
-    def __init__(self, LIB = 'numpy'): 
+class LieAbstract(ABC):
+    @abstractmethod
+    def __init__(self, LIB : str = 'numpy'): 
         '''
         Constructor
         '''
-        pass
+        self._lib = LIB
+
+    @property
+    @abstractmethod
+    def lib(self) -> str:
+        '''
+        Return the library used for the Lie group
+        '''
+        return self._lib
 
     @staticmethod
-    def hat(vec, LIB = 'numpy'):
+    @abstractmethod
+    def dof() -> int:
+        '''
+        Return the degree of freedom of the Lie group
+        '''
+        pass
+
+    @property
+    @abstractmethod
+    def mat_size(self) -> int:
+        '''
+        Return the size of the matrix representation of the Lie group
+        '''
+        pass
+
+    @abstractmethod
+    def mat(self) -> Union[np.ndarray, jnp.ndarray]:
+        '''
+        Return the matrix representation of the Lie group
+        '''
+        pass
+
+    @abstractmethod
+    def set_mat(self, mat : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> 'LieAbstract':
+        '''
+        Set the matrix representation of the Lie group
+        '''
+        pass
+    
+    @staticmethod
+    @abstractmethod
+    def hat(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         '''
         hat operator on the tanget space vector
         '''
         pass
     
     @staticmethod
-    def hat_commute(vec, LIB = 'numpy'):
+    def hat_commute(vec  : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         '''
         hat commute operator on the tanget space vector
         hat(a) @ b = hat_commute(b) @ a 
@@ -24,7 +68,8 @@ class LieAbstract:
         pass
 
     @staticmethod
-    def vee(vec_hat, LIB = 'numpy'):
+    @abstractmethod
+    def vee(vec_hat : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         '''
         a = vee(hat(a))
         '''
@@ -38,8 +83,10 @@ class LieAbstract:
     def exp_integ(vec, a, LIB = 'numpy'):
         pass
     
+    @abstractmethod
     def inv(self):
         pass
+    
     
     def mat_adj(self):
         '''
