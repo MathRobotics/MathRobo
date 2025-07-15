99cd60bbf88a4c0d3af2f999eba773231a4c8fc4
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-07-15 23:19:37 +0900

    💡IMPROVE:

--- mathrobo/lie/so3.py
+++ mathrobo/lie/so3.py
@@ -1,9 +1,10 @@
-from ..basic import *
 from .lie_abst import *
+
+from typing import Union
 
 class SO3(LieAbstract):
     _dof = 3
-    def __init__(self, r = np.identity(3), LIB = 'numpy'):
+    def __init__(self, r = np.identity(3), LIB : str = 'numpy'):
         '''
         Constructor
         '''
@@ -22,18 +23,18 @@ class SO3(LieAbstract):
         return 3
     
     @staticmethod
-    def mat_size():
+    def mat_size() -> int:
         return 3
     
     @staticmethod
-    def mat_adj_size():
+    def mat_adj_size() -> int:
         return 3
         
-    def mat(self):
+    def mat(self) -> Union[np.ndarray, jnp.ndarray]:
         return self._rot
     
     @staticmethod
-    def set_mat(mat = np.identity(3), LIB = 'numpy'):
+    def set_mat(mat = np.identity(3), LIB : str = 'numpy') -> 'SO3':
         return SO3(mat, LIB)
     
     def quaternion(self) -> np.ndarray:
@@ -83,7 +84,7 @@ class SO3(LieAbstract):
         return q  # [w, x, y, z]
         
     @staticmethod
-    def quaternion_to_mat(quaternion, LIB = 'numpy'):
+    def quaternion_to_mat(quaternion : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         w, x, y, z = quaternion
         if LIB == 'jax':
             m = jnp.array([
@@ -103,88 +104,107 @@ class SO3(LieAbstract):
             raise ValueError("Unsupported library. Choose 'numpy' or 'jax'.")
 
     @staticmethod
-    def set_quaternion(quaternion, LIB = 'numpy'):
+    def set_quaternion(quaternion : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> 'SO3':
         return SO3(SO3.quaternion_to_mat(quaternion), LIB)
     
     @staticmethod
-    def mat_to_quaternion(mat, LIB = 'numpy'):
+    def mat_to_quaternion(mat : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         m = SO3(mat, LIB)
         return m.quaternion()
     
     @staticmethod
-    def eye(LIB = 'numpy'):
-        return SO3(identity(3), LIB)
+    def eye(LIB : str = 'numpy') -> 'SO3':
+        if LIB == 'jax':
+            return SO3(jnp.identity(3), LIB)
+        elif LIB == 'numpy':
+            return SO3(np.identity(3), LIB)
+        else:
+            raise ValueError("Unsupported library. Choose 'numpy' or 'jax'.")
 
-    def inv(self):
+    def inv(self) -> 'SO3':
         return SO3(self._rot.transpose(), self.lib)
-        
-    def mat_inv(self):
+
+    def mat_inv(self) -> Union[np.ndarray, jnp.ndarray]:
         return self._rot.transpose()
 
-    def mat_adj(self):
+    def mat_adj(self) -> Union[np.ndarray, jnp.ndarray]:
         return self._rot
     
     @staticmethod
-    def set_mat_adj(mat = identity(3), LIB = 'numpy'):
+    def set_mat_adj(mat = np.identity(3), LIB : str = 'numpy') -> 'SO3':
         return SO3(mat, LIB)
 
-    def mat_inv_adj(self):
+    def mat_inv_adj(self) -> Union[np.ndarray, jnp.ndarray]:
         return self._rot.transpose()
 
     @staticmethod
-    def hat(vec, LIB = 'numpy'):
+    def hat(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         if LIB == "jax":
             vx, vy, vz = vec
             return jnp.array([
                             [   0, -vz,  vy],
                             [  vz,   0, -vx],
                             [ -vy,  vx,   0]], dtype=vec.dtype)
-  
-        mat = zeros((3,3), LIB)
-        mat[1,2] = -vec[0]
-        mat[2,1] =  vec[0]
-        mat[2,0] = -vec[1]
-        mat[0,2] =  vec[1]
-        mat[0,1] = -vec[2]
-        mat[1,0] =  vec[2]
-
-        return mat
+        elif LIB == "numpy":
+            vx, vy, vz = vec
+            return np.array([
+                            [   0, -vz,  vy],
+                            [  vz,   0, -vx],
+                            [ -vy,  vx,   0]], dtype=vec.dtype)
+        else:
+            raise ValueError("Unsupported library. Choose 'numpy' or 'jax'.")
     
     @staticmethod
-    def hat_commute(vec, LIB = 'numpy'):
+    def hat_commute(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return -SO3.hat(vec, LIB)
 
     @staticmethod
-    def vee(vec_hat, LIB = 'numpy'):
+    def vee(vec_hat : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         if LIB == 'jax':
             return jnp.array([
                 0.5 * (vec_hat[2, 1] - vec_hat[1, 2]),
                 0.5 * (vec_hat[0, 2] - vec_hat[2, 0]),
                 0.5 * (vec_hat[1, 0] - vec_hat[0, 1])
             ], dtype=vec_hat.dtype)
-        vec = zeros(3, LIB)
-        vec[0] = (-vec_hat[1,2] + vec_hat[2,1]) / 2
-        vec[1] = (-vec_hat[2,0] + vec_hat[0,2]) / 2
-        vec[2] = (-vec_hat[0,1] + vec_hat[1,0]) / 2
-        return vec 
+        elif LIB == 'numpy':
+            return np.array([
+                0.5 * (vec_hat[2, 1] - vec_hat[1, 2]),
+                0.5 * (vec_hat[0, 2] - vec_hat[2, 0]),
+                0.5 * (vec_hat[1, 0] - vec_hat[0, 1])
+            ], dtype=vec_hat.dtype)
+        else:
+            raise ValueError("Unsupported library. Choose 'numpy' or 'jax'.")
 
     @staticmethod
-    def exp(vec, a = 1., LIB = 'numpy'):
-        """
-            回転行列の計算
-            sympyの場合,vecの大きさは1を想定
-        """
+    def exp(vec : Union[np.ndarray, jnp.ndarray], a : float = 1., LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         if LIB == 'numpy':
-            theta = norm(vec, LIB)
-            if not isclose(theta, 1.0, LIB):
+            theta = np.linalg.norm(vec)
+            if not math.isclose(theta, 1.0):
                 a_ = a*theta
             else:
                 a_ = a
-                
-            if iszero(theta):
-                return identity(3, LIB)
+
+            if math.isclose(theta, 0):
+                return np.identity(3)
             else:
-                x, y, z = vec / theta           
+                x, y, z = vec / theta
+
+            sa = np.sin(a_)
+            ca = np.cos(a_)
+
+            mat = np.zeros((3,3))
+
+            mat[0,0] = ca + (1-ca)*x*x
+            mat[0,1] = (1-ca)*x*y - sa*z
+            mat[0,2] = (1-ca)*x*z + sa*y
+            mat[1,0] = (1-ca)*y*x + sa*z
+            mat[1,1] = ca + (1-ca)*y*y
+            mat[1,2] = (1-ca)*y*z - sa*x
+            mat[2,0] = (1-ca)*z*x - sa*y
+            mat[2,1] = (1-ca)*z*y + sa*x
+            mat[2,2] = ca + (1-ca)*z*z
+
+            return mat
         elif LIB == 'jax':
             n = jnp.linalg.norm(vec)
             a_  = n * a
@@ -198,49 +218,43 @@ class SO3(LieAbstract):
             K = SO3.hat(vec/n, 'jax')
 
             return I + A * K + B * (K @ K)
-        elif LIB == 'sympy':
-            a_ = a
-            x = vec[0]
-            y = vec[1]
-            z = vec[2]
         else:
             raise ValueError("Unsupported library. Choose 'numpy' or 'sympy'.")
-
-        sa = sin(a_, LIB)
-        ca = cos(a_, LIB)
-
-        mat = zeros((3,3), LIB)
-
-        mat[0,0] = ca + (1-ca)*x*x
-        mat[0,1] = (1-ca)*x*y - sa*z
-        mat[0,2] = (1-ca)*x*z + sa*y
-        mat[1,0] = (1-ca)*y*x + sa*z
-        mat[1,1] = ca + (1-ca)*y*y
-        mat[1,2] = (1-ca)*y*z - sa*x
-        mat[2,0] = (1-ca)*z*x - sa*y
-        mat[2,1] = (1-ca)*z*y + sa*x
-        mat[2,2] = ca + (1-ca)*z*z
-
-        return mat
     
     @staticmethod
-    def exp_integ(vec, a = 1., LIB = 'numpy'):
-        """
-            回転行列の積分の計算
-            sympyの場合,vecの大きさは1を想定
-        """
+    def exp_integ(vec : Union[np.ndarray, jnp.ndarray], a : float = 1., LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         if LIB == 'numpy':
-            theta = norm(vec, LIB)
-            if not isclose(theta, 1.0, LIB):
+            theta = np.linalg.norm(vec)
+            if not math.isclose(theta, 1.0):
                 a_ = a*theta
             else:
                 a_ = a
 
-            if iszero(theta):
-                return a*identity(3)
+            if math.isclose(theta, 0):
+                return a*np.identity(3)
             else:
                 x, y, z = vec/theta
                 k = 1./theta
+
+            sa = np.sin(a_)
+            ca = np.cos(a_)
+
+            mat = np.zeros((3,3))
+
+            u = a_-sa
+            v = (1-ca)
+
+            mat[0,0] = k*(sa + u*x*x)
+            mat[0,1] = k*(u*x*y - v*z)
+            mat[0,2] = k*(u*z*x + v*y)
+            mat[1,0] = k*(u*x*y + v*z)
+            mat[1,1] = k*(sa + u*y*y)
+            mat[1,2] = k*(u*y*z - v*x)
+            mat[2,0] = k*(u*z*x - v*y)
+            mat[2,1] = k*(u*y*z + v*x)
+            mat[2,2] = k*(sa + u*z*z)
+
+            return mat
 
         elif LIB == 'jax':
             n = jnp.linalg.norm(vec)
@@ -255,103 +269,71 @@ class SO3(LieAbstract):
             K = SO3.hat(vec, 'jax')
 
             return a_*I + A * K + B * (K @ K)
-                
-        elif LIB == 'sympy':
-            a_ = a
-            x, y, z = vec
-            k = 1.
         else:
-            raise ValueError("Unsupported library. Choose 'numpy' or 'sympy'.")
-
-        sa = sin(a_, LIB)
-        ca = cos(a_, LIB)
-
-        mat = zeros((3,3), LIB)
-        
-        u = a_-sa
-        v = (1-ca)
-
-        mat[0,0] = k*(sa + u*x*x)
-        mat[0,1] = k*(u*x*y - v*z)
-        mat[0,2] = k*(u*z*x + v*y)
-        mat[1,0] = k*(u*x*y + v*z)
-        mat[1,1] = k*(sa + u*y*y)
-        mat[1,2] = k*(u*y*z - v*x)
-        mat[2,0] = k*(u*z*x - v*y)
-        mat[2,1] = k*(u*y*z + v*x)
-        mat[2,2] = k*(sa + u*z*z)
-
-        return mat
+            raise ValueError("Unsupported library. Choose 'numpy' or 'jax'.")
     
     @staticmethod
-    def exp_integ2nd(vec, a = 1., LIB = 'numpy'):
-        """
-            回転行列の積分の計算
-            sympyの場合,vecの大きさは1を想定
-        """
-        if LIB == 'numpy' or LIB == 'jax':
-            theta = norm(vec, LIB)
-            if not isclose(theta, 1.0, LIB):
+    def exp_integ2nd(vec : Union[np.ndarray, jnp.ndarray], a : float = 1., LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
+        if LIB == 'numpy':
+            theta = np.linalg.norm(vec)
+            if not math.isclose(theta, 1.0):
                 a_ = a*theta
             else:
                 a_ = a
 
-            if iszero(theta):
-                return a*identity(3, LIB)
+            if math.isclose(theta, 0):
+                return a*np.identity(3)
             else:
                 x, y, z = vec/theta
                 k = 1./(theta*theta)
-                
-        elif LIB == 'sympy':
-            a_ = a
-            x, y, z = vec
-            k = 1.
+
+            sa = np.sin(a_)
+            ca = np.cos(a_)
+
+            mat = np.zeros((3,3))
+
+            u = 1-ca
+            v = a_-sa
+            w = 0.5*a_**2-1+ca
+
+            mat[0,0] = k*(u  + w*x*x)
+            mat[0,1] = k*(w*x*y - v*z)
+            mat[0,2] = k*(w*z*x + v*y)
+            mat[1,0] = k*(w*x*y + v*z)
+            mat[1,1] = k*(u  + w*y*y)
+            mat[1,2] = k*(w*y*z - v*x)
+            mat[2,0] = k*(w*z*x - v*y)
+            mat[2,1] = k*(w*y*z + v*x)
+            mat[2,2] = k*(u  + w*z*z)
+            
+            return mat
         else:
-            raise ValueError("Unsupported library. Choose 'numpy' or 'sympy'.")
-
-        sa = sin(a_, LIB)
-        ca = cos(a_, LIB)
-
-        mat = zeros((3,3), LIB)
-        
-        u = 1-ca
-        v = a_-sa
-        w = 0.5*a_**2-1+ca
-
-        mat[0,0] = k*(u  + w*x*x)
-        mat[0,1] = k*(w*x*y - v*z)
-        mat[0,2] = k*(w*z*x + v*y)
-        mat[1,0] = k*(w*x*y + v*z)
-        mat[1,1] = k*(u  + w*y*y)
-        mat[1,2] = k*(w*y*z - v*x)
-        mat[2,0] = k*(w*z*x - v*y)
-        mat[2,1] = k*(w*y*z + v*x)
-        mat[2,2] = k*(u  + w*z*z)
-        
-        return mat
+            raise ValueError("Unsupported library. Choose 'numpy'.")
     
     @staticmethod
-    def hat_adj(vec, LIB = 'numpy'):
+    def hat_adj(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.hat(vec, LIB)
     
     @staticmethod
-    def hat_commute_adj(vec, LIB = 'numpy'):
+    def hat_commute_adj(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.hat_commute(vec, LIB)
     
     @staticmethod
-    def vee_adj(mat, LIB = 'numpy'):
+    def vee_adj(mat : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.vee(mat, LIB)
     
     @staticmethod
-    def exp_adj(vec, a, LIB = 'numpy'):
+    def exp_adj(vec : Union[np.ndarray, jnp.ndarray], a : float, LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.exp(vec, a, LIB)
     
     @staticmethod
-    def exp_integ_adj(vec, a, LIB = 'numpy'):
+    def exp_integ_adj(vec : Union[np.ndarray, jnp.ndarray], a : float, LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.exp_integ(vec, a, LIB)
 
     @staticmethod
-    def sub_tan_vec(val0, val1, type = 'bframe', LIB = 'numpy'):
+    def sub_tan_vec(val0 : Union[np.ndarray, jnp.ndarray], 
+                    val1 : Union[np.ndarray, jnp.ndarray], 
+                    type : str = 'bframe', LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         if type == 'bframe':
             vec = SO3.vee(val0.mat_inv() @ (val1._rot - val0._rot), LIB)
         elif type == 'fframe':
@@ -359,7 +341,7 @@ class SO3(LieAbstract):
         return vec
     
     @staticmethod
-    def so3_mul(l_rot, r_rot):  
+    def so3_mul(l_rot : Union[np.ndarray, jnp.ndarray], r_rot : Union[np.ndarray, jnp.ndarray]) -> Union[np.ndarray, jnp.ndarray]:
         return l_rot @ r_rot
 
     def __matmul__(self, rval):
@@ -371,31 +353,31 @@ class SO3(LieAbstract):
             TypeError("Right operand should be SO3 or numpy.ndarray")
 
     @staticmethod
-    def rand(LIB = 'numpy'):
+    def rand(LIB : str = 'numpy') -> 'SO3':
         v = np.random.rand(3) 
         m = SO3.exp(v)
         return SO3(m, LIB)
     
 class SO3wrench(SO3):
     @staticmethod
-    def hat(vec, LIB = 'numpy'):
+    def hat(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return -SO3.hat(vec, LIB)
     
     @staticmethod
-    def hat_commute(vec, LIB = 'numpy'):
+    def hat_commute(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.hat(vec, LIB)
     
     @staticmethod
-    def exp(vec, a, LIB = 'numpy'):
+    def exp(vec : Union[np.ndarray, jnp.ndarray], a : float, LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.exp(vec, a, LIB).transpose()
     
     @staticmethod
-    def exp_integ(vec, a, LIB = 'numpy'):
+    def exp_integ(vec : Union[np.ndarray, jnp.ndarray], a : float, LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.exp_integ(vec, a, LIB).transpose()
     
 class SO3inertia(SO3):
     @staticmethod
-    def hat(vec, LIB = 'numpy'):
+    def hat(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         mat = zeros((3,3), LIB)
 
         mat[0,0] = vec[0]
@@ -411,7 +393,7 @@ class SO3inertia(SO3):
         return mat
     
     @staticmethod
-    def hat_commute(vec, LIB = 'numpy'):
+    def hat_commute(vec : Union[np.ndarray, jnp.ndarray], LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         mat = zeros((3, 6), LIB)
 
         mat[0,0] = vec[0]
@@ -428,9 +410,9 @@ class SO3inertia(SO3):
         return mat
     
     @staticmethod
-    def exp(vec, a, LIB = 'numpy'):
+    def exp(vec : Union[np.ndarray, jnp.ndarray], a : float, LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.exp(vec, a, LIB).transpose()
     
     @staticmethod
-    def exp_integ(vec, a, LIB = 'numpy'):
+    def exp_integ(vec : Union[np.ndarray, jnp.ndarray], a : float, LIB : str = 'numpy') -> Union[np.ndarray, jnp.ndarray]:
         return SO3.exp_integ(vec, a, LIB).transpose()
