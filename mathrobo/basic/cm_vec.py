b33be73a77eccebee07d067912f639c8707a6580
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-10-30 18:39:13 +0900

    🐛 FIX:

--- mathrobo/basic/cm_vec.py
+++ mathrobo/basic/cm_vec.py
@@ -9,6 +9,9 @@ from .factorial_vec import *
 class CMVector:
     def __init__(self, vecs : Union[np.ndarray, jnp.ndarray]):
         self._factorial_vector = FactorialVector(vecs)
+        self._n = self._factorial_vector._n
+        self._dim = self._factorial_vector._dim
+        self._len = self._factorial_vector._len
 
     def vecs(self) -> Union[np.ndarray, jnp.ndarray]:
         return self._factorial_vector.vecs()
