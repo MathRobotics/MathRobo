80d9d6be85799c63ba0d33c79bd408f6e5985fad
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-10-30 17:10:32 +0900

    👌 IMPROVE:

--- tests/basic/test_factorial_vec.py
+++ tests/basic/test_factorial_vec.py
@@ -11,23 +11,23 @@ def test_factorial_vector():
     factorial_vector = mr.FactorialVector(vecs)
     assert factorial_vector.vecs().shape == (test_order,test_dim)
     assert factorial_vector.vec().shape == (test_dim*test_order,)
-    assert factorial_vector.vec_factorial().shape == (test_dim*test_order,)
+    assert factorial_vector.fac_vec().shape == (test_dim*test_order,)
 
     np.testing.assert_allclose(factorial_vector.vecs(), vecs)
     for i in range(test_order):
-        np.testing.assert_allclose(factorial_vector.vecs(i), vecs[i])
+        np.testing.assert_allclose(factorial_vector.vecs()[i], vecs[i])
     for i in range(test_order):
         np.testing.assert_allclose(factorial_vector.vecs()[i:], vecs[i:])
     np.testing.assert_allclose(factorial_vector.vec(), vecs.flatten())
 
     expected = np.array([math.factorial(i) * vecs[i] for i in range(test_order)])
-    np.testing.assert_allclose(factorial_vector.vec_factorial(), expected.flatten())
+    np.testing.assert_allclose(factorial_vector.fac_vec(), expected.flatten())
 
 def test_factorial_matrix():
     vec = np.random.rand(test_order)
     factorial_vector = mr.FactorialVector(vec)
-    factorial_mat = factorial_vector.factorial_mat()
-    inverse_factorial_mat = factorial_vector.inverse_factorial_mat()
+    factorial_mat = factorial_vector.fac_mat()
+    inverse_factorial_mat = factorial_vector.ifac_mat()
 
     identity_mat = factorial_mat @ inverse_factorial_mat
     np.testing.assert_allclose(identity_mat, np.eye(test_order))
