549bd658c9697b26b229f999efc8bdaa330ad707
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-10-30 14:45:39 +0900

    👌 IMPROVE:

--- mathrobo/basic/factorial_vec.py
+++ mathrobo/basic/factorial_vec.py
@@ -6,24 +6,24 @@ import jax.numpy as jnp
 
 class FactorialVector:
     def __init__(self, vecs : Union[np.ndarray, jnp.ndarray]):
-        self.n = vecs.shape[0]
-        self.dim = vecs.shape[1] if len(vecs.shape) > 1 else 1
-        self.len = vecs.flatten().shape[0]
-        self.vecs = vecs
+        self._n = vecs.shape[0]
+        self._dim = vecs.shape[1] if len(vecs.shape) > 1 else 1
+        self._len = vecs.flatten().shape[0]
+        self._vecs = vecs
         self.factorial_mat = self._compute_factorial_matrix()
 
     def _compute_factorial_matrix(self) -> Union[np.ndarray, jnp.ndarray]:
-        if isinstance(self.vecs, np.ndarray):
-            mat = np.eye(self.len, dtype=self.vecs.dtype)
+        if isinstance(self._vecs, np.ndarray):
+            mat = np.eye(self._len, dtype=self._vecs.dtype)
         else:
-            mat = jnp.eye(self.len, dtype=self.vecs.dtype)
+            mat = jnp.eye(self._len, dtype=self._vecs.dtype)
 
-        for i in range(self.n):
-            mat[i*self.dim:(i+1)*self.dim, i*self.dim:(i+1)*self.dim] *= math.factorial(i)
+        for i in range(self._n):
+            mat[i*self._dim:(i+1)*self._dim, i*self._dim:(i+1)*self._dim] *= math.factorial(i)
         return mat
 
     def vec(self) -> Union[np.ndarray, jnp.ndarray]:
-        return self.vecs.flatten()
-    
+        return self._vecs.flatten()
+
     def vec_factorial(self) -> Union[np.ndarray, jnp.ndarray]:
-        return self.factorial_mat @ self.vec()
+        return self.factorial_mat @ self._vecs.flatten()
